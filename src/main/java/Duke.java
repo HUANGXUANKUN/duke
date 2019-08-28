c7617ef029a5ce1cc38832b0550e262de979bc4c
HUANG XUAN KUN <hxk123123123@gmail.com>
2019-08-28 23:08:23 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,22 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from I'm Duke\n" + logo);
+        System.out.println("What can I do for you?");
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            String userInput;
+            userInput = input.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            else{
+                System.out.println(userInput);
+            }
+        }
     }
 }
