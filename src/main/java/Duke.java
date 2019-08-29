a396dbf4a55175973f8590290dddf8419d2da7c3
HUANG XUAN KUN <hxk123123123@gmail.com>
2019-08-29 19:22:59 +0800

    Level-4 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,48 +2,62 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from I'm Duke\n" + logo);
-        System.out.println("What can I do for you?");
         Scanner input = new Scanner(System.in);
+        String greeting = "Hello from I'm Duke\n" +
+                "What can I do for you?";
+        String longDash = "_______________________________";
+        System.out.println(greeting);
 
         ArrayList<Task> taskList = new ArrayList<Task>();
-        int taskNumber = 0;
+
         while (true) {
-            String userInput;
-            userInput = input.nextLine();
+            String userInput = input.nextLine();
 
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            }
-
-            else if (userInput.equals("list")) {
-                for (int i = 0; i < taskNumber; i++) {
-                    Task task = taskList.get(i);
-                    System.out.println((i + 1) + ". " + "[" + task.getStatusIcon() + "]" + task.description);
+            } else if (userInput.startsWith("done")) {
+                String[] textList = userInput.split(" ");
+                int taskIndex = Integer.parseInt(textList[1]) - 1;
+                Task t = taskList.get(taskIndex);
+                t.markAsDone();
+                System.out.println("Nice! I've marked this task as done: \n");
+                System.out.println("[" + t.getStatusIcon() + "]" + t.description);
+            } else if (userInput.equals("list")) {
+                System.out.println(longDash + "\n" + "Here are the tasks in your list:");
+                for (int i = 0; i < taskList.size(); i++) {
+                    Task t = taskList.get(i);
+                    System.out.println((i + 1) + ". " + t.toString());
                 }
-            }
+            } else if (userInput.startsWith("Deadline") || userInput.startsWith("Event") || userInput.startsWith("Todo")) {
+                String[] textArr = userInput.split(" ", 2);
 
-            else if (userInput.startsWith("Done")) {
-                    int num = userInput.charAt((userInput.length()) - 1) - '0';
-                    Task t = taskList.get(num - 1);
-                    t.markAsDone();
-                    System.out.println("Nice! I've marked this task as done: \n");
-                    System.out.println("[" + t.getStatusIcon() + "]" + t.description);
-            }
-
-            else {
-                    taskNumber++;
-                    Task t = new Task(userInput);
-                    taskList.add(t);
-                    System.out.println("added: " + userInput);
+                if (userInput.startsWith("Deadline")) {
+                    String[] contentArr = textArr[1].split("/by");
+                    String description = contentArr[0];
+                    String by = contentArr[1];
+                    Deadline deadline = new Deadline(description, by);
+                    taskList.add(deadline);
+                } else if (userInput.startsWith("Event")) {
+                    String[] contentArr = textArr[1].split("/at");
+                    String description = contentArr[0];
+                    String at = contentArr[1];
+                    Event event = new Event(description, at);
+                    taskList.add(event);
+                } else {
+                    Todo todo = new Todo(textArr[1]);
+                    taskList.add(todo);
                 }
+
+                System.out.println(longDash);
+                System.out.println("Got it. I've added this task: \n");
+                System.out.println(taskList.get(taskList.size() - 1).toString());
+                System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+                System.out.println(longDash);
             }
         }
+
+    }
 }
