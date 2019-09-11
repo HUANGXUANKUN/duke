33616851eef9de7602e6fe3557f9ba8ea6a25438
HUANG XUAN KUN <hxk123123123@gmail.com>
2019-09-11 17:16:43 +0800

    Updated DateFormatterTest to test invalid Date&Time input

--- src/test/java/duke/core/DateFormatterTest.java
+++ src/test/java/duke/core/DateFormatterTest.java
@@ -7,23 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DateFormatterTest {
     @Test
     public void formattingTest1(){
-        assertEquals("1st of January 1996, 10AM", DateFormatter.convertToDateAndTime("01/01/1996 1035"));
+        assertEquals("1st of January 1996, 10AM", DateFormatter.convertToDateAndTime("01/01/1996 1035"), "The date&time is not converted into proper format");
+        assertEquals("22nd of February 1996, 12AM", DateFormatter.convertToDateAndTime("22/02/1996 0000"), "The date&time is not converted into proper format");
+        assertEquals("3rd of June 1996, 5PM", DateFormatter.convertToDateAndTime("03/06/1996 1735"),"The date&time is not converted into proper format");
+        assertEquals("14th of December 1996, 11PM", DateFormatter.convertToDateAndTime("14/12/1996 2359"),"The date&time is not converted into proper format");
     }
 
     @Test
     public void formattingTest2(){
-        assertEquals("22nd of February 1996, 12AM", DateFormatter.convertToDateAndTime("22/02/1996 0000"));
+        assertEquals("1/01/2000 1035", DateFormatter.convertToDateAndTime("1/01/2000 1035"), "The date format should be invalid");
+        assertEquals("1/01/2000 1035", DateFormatter.convertToDateAndTime("1/01/2000 1035"), "The date format should be invalid");
+        assertEquals("01/01/2000 2500", DateFormatter.convertToDateAndTime("01/01/2000 2500"), "Invalid time is given");
     }
 
-    @Test
-    public void formattingTest3(){
-        assertEquals("3rd of June 1996, 5PM", DateFormatter.convertToDateAndTime("03/06/1996 1735"));
-    }
-
-    @Test
-    public void formattingTest4(){
-        assertEquals("14th of December 1996, 11PM", DateFormatter.convertToDateAndTime("14/12/1996 2359"));
-    }
 
 
 
